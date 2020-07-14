fffc742b24ab0eebd41935cf087cdf4ce2efabdb
Parcifal <scar1987@gmail.com>
2020-07-14 22:43:44 +0800

    Initial Commit

---
+++ PavelRazumnikov/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PavelRazumnikov
+//
+//  Created by Павел on 14.07.2020.
+//  Copyright © 2020 Павел. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
